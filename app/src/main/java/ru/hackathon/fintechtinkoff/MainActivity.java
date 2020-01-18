204002c79c736bd2095dd6b1abb6f7aaf54427e1
Alekseych4 <alekseych@list.ru>
2020-01-18 09:24:43 +0300

    Initial commit, RecyclerView and its item template were added

---
+++ app/src/main/java/ru/hackathon/fintechtinkoff/MainActivity.java
@@ -0,0 +1,17 @@
+package ru.hackathon.fintechtinkoff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView newsRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        newsRecyclerView = findViewById(R.id.news_list);
+    }
+}
